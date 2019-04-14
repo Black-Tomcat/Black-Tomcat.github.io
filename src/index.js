69bb79e80f36f9d680b7cc77cf3e02720ecb88d0
Black-Tomcat <tomcat.frantz@gmail.com>
2019-04-14 21:36:56 +1000

    Basic addition of React Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Route, Link, Switch} from "react-router-dom";
+
+// PAGES
+import About from "./containers/about";
+import Index from "./containers";
 
 
 class App extends Component {
     render() {
         return (
-            <div>The Wombat And The Black Tomcat</div>
+            <BrowserRouter>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About Me</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div>The Wombat And The Black Tomcat</div>
+
+                    <Route path="/" exact component={Index}/>
+                    <Route path="/about" component={About}/>
+                </div>
+            </BrowserRouter>
         )
     }
 }
