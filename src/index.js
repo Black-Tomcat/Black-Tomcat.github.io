56183fbebbd29dfbaa3f7f6c42712aef94cfee8f
Black-Tomcat <tomcat.frantz@gmail.com>
2019-04-14 21:08:06 +1000

    Initial source added for page

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>The Wombat And The Black Tomcat</div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById("react-entry"));
